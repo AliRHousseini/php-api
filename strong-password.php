11bf74a3f91556a9e162ece3e5e86df75199a21a
Ali Husseini <macbookpro@Apples-MacBook-Pro.local>
2022-09-13 09:25:57 +0300

    check if password is strong

---
+++ strong-password.php
@@ -0,0 +1,15 @@
+<?php
+$password = $_POST["password"];
+
+$uppercase = preg_match('@[A-Z]@', $password);
+$lowercase = preg_match('@[a-z]@', $password);
+$number    = preg_match('@[0-9]@', $password);
+$specialChars = preg_match('@[^\w]@', $password);
+
+if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
+    echo 'choose a stronger password';
+}else{
+    echo 'Strong password.';
+}
+
+?>
